06e3a43fa894b9288829732ec2fa07a345e620f0
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 14:54:18 -0800

    Char-rep-count

---
+++ check_syntax_error.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * repeated_char - counts the repetitions of a char
+ *
+ * @input: input string
+ * @i: index
+ * Return: repetitions
+ */
+
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
