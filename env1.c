0ea3a3afb2a80544ae9ba8e683489d20f1e759d9
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 14:40:52 -0800

    Environ Compare

---
+++ env1.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * cmp_env_name - compares env variables names
+ * with the name passed.
+ * @nenv: name of the environment variable
+ * @name: name passed
+ *
+ * Return: 0 if are not equal. Another value if they are.
+ */
+
+int cmp_env_name(const char *nenv, const char *name)
+{
+	int i;
