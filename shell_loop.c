9af9506156dac684698c0ef0282973cd7f24b3d1
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 12:34:17 -0800

    Remover

---
+++ shell_loop.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * without_comment - deletes comments from the input
+ *
+ * @in: input string
+ *
+ * Return: input without comments
+ */
+
+char *without_comment(char *in)
