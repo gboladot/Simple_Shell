839561314ae3a13949e97b2dbebb2ae7fea46c50
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 12:27:18 -0800

    Split

---
+++ split.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_char - swaps | and & for non-printed chars
+ *
+ * @input: input string
+ * @bool: type of swap
+ *
+ * Return: swapped string
+ */
+
+char *swap_char(char *input, int bool)
