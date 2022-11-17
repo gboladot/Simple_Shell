ef82432e9099eefc456ff377daf7b46b436567a3
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 20:07:29 -0800

    String concatenation

---
+++ aux_str.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char pointer the dest of the copied str
+ * @src: const char pointer the source of str
+ *
+ * Return: the dest
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+	int i;
+	int j;
