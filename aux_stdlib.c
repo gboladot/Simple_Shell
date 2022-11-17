07ff20972abc9f48aee1aa39d0becc786553e75f
Azeez Ahmed <azeezgboladele@gmail.com>
2022-11-16 20:12:15 -0800

    Number lenght

---
+++ aux_stdlib.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ *
+ * Return: Lenght of a number.
+ */
+
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+
